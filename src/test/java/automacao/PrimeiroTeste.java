faf1e94fe234475fce38a950d3129d99c7ee6cb0
C1323211 Jose Raimundo de Sa Filho <c1323211@interno.bb.com.br>
2022-04-15 01:46:22 -0300

    Automatizando teste de API com Rest Assured

---
+++ src/test/java/automacao/PrimeiroTeste.java
@@ -0,0 +1,55 @@
+package automacao;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class PrimeiroTeste {
+	
+	@BeforeClass
+	public static void setUp() {
+		/* Caso o teste falhe o Rest Assured irá habilitar o Log de resposta de requisições*/
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	}
+
+	@Test
+	public void testeListaMetadadosUsuario() {
+		/* A Ação */
+		when()
+			.get("https://reqres.in/api/users?page=2")
+		.then()
+		.statusCode(200)
+		.statusCode(HttpStatus.SC_OK)
+		.body("page", is(2))
+		.body("total", is(12))
+		.body("data[0].email", is("michael.lawson@reqres.in"))
+		.body("data.size", is(6))
+		.body("data", is(notNullValue()));
+	}
+	
+	@Test
+	public void testeCriarUsuarioComSucesso() {
+		/* Preparando a ação */
+		given()
+		.body("{\"name\": \"Johnson\", \"job\": \"motorista\"}")
+		.contentType(ContentType.JSON)
+//			.params("name", "Johnson")
+//			.params("job", "motorista")
+		
+		.when()
+			.post("https://reqres.in/api/users")
+		.then()
+			.statusCode(HttpStatus.SC_CREATED)
+			.body("name", is("Johnson"))
+			.body("id", is(notNullValue()))
+			.body("createdAt", is(notNullValue()))
+			.body("job", is("motorista"));
+	}
+	
+}

5e41a03fc9bca4d86ad8b6eada4cbf1770dbdaa5
C1323211 Jose Raimundo de Sa Filho <c1323211@interno.bb.com.br>
2022-04-15 22:34:44 -0300

    Usando o Groovy collection, o extract do RestAssured e refatorando

--- src/test/java/automacao/api/teste/UsuarioTeste.java
+++ src/test/java/automacao/api/teste/UsuarioTeste.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import automacao.api.dominio.Usuario;
 
-public class UsuárioTeste extends BaseTeste{
+public class UsuarioTeste extends BaseTeste{
 
 	/* final quer dizer que uma vez que ela for definida não terá como mudá-la programaticamente */
 	private static final String LISTA_USUARIOS_ENDPOINTS = "/users";
@@ -68,4 +68,43 @@ public class UsuárioTeste extends BaseTeste{
 	.body("data.first_name", is("Janet"));
 	}
 	
+	@Test
+	public void testeTamanhoDosItensMostradosIgualAoPerPage() {
+		int paginaEsperada = 2;
+		int perPageEsperado = retornaPerPageEsperado(paginaEsperada);
+		
+		given().
+			params("page", paginaEsperada).
+		when().
+			get(LISTA_USUARIOS_ENDPOINTS).
+		then().
+			statusCode(HttpStatus.SC_OK).
+			body(
+					"page", is(paginaEsperada),
+					/* Usando o Groovy collection */
+					"data.size()", is(perPageEsperado),
+					/* Comando do Groovy. O it é de iterator (faz um For de todos
+					 * os itens do data). Cada item será um it. O comando startsWith() é
+					 * para verificar se o dado do item começa com um valor determinado */
+					"data.findAll { it.avatar.startsWith('https://reqres.in/img/faces') }.size()", is(perPageEsperado)
+					//Acima verifica se o size está correto.
+			);
+	}
+
+	private int retornaPerPageEsperado(int pagina) {
+		/* Funcionalidade Extract.
+		 * Como quero o perPage e ele é um inteiro, declaro uma variável int.
+		 * Para garantir que foi feito a solicitação já colocamos um statusCode.
+		 * Após a solicitação nós informamos no extract que queremos extrair o body,
+		 * quero extrair um path e o declaramos. Ele retorna e coloca na variável. */
+		int perPageEsperado = given().
+				params("page", pagina).
+			when().
+				get(LISTA_USUARIOS_ENDPOINTS).
+			then().
+				statusCode(HttpStatus.SC_OK).
+			extract().
+				path("per_page");
+		return perPageEsperado;
+	}	
 }
